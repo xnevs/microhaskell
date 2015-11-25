bc04ef8abb2ea48644f8aa63e7dab0675f718f94
Sven Cerk <sven.cerk@gmail.com>
2015-11-25 18:51:25 +0100

    ...

--- homework2/LLMH_Evaluator.hs
+++ homework2/LLMH_Evaluator.hs
@@ -21,10 +21,10 @@ evaluate env (Boolean b) = (Boolean b)
 
 evaluate env (Cond(exp0, exp1, exp2)) =
   let val0 = evaluate env exp0
-   in case val0 of
-        (Boolean True) -> evaluate env exp1
-        (Boolean False) -> evaluate env exp2
-        _ -> error "Runtime type error"
+  in case val0 of
+    (Boolean True) -> evaluate env exp1
+    (Boolean False) -> evaluate env exp2
+    _ -> error "Runtime type error"
 
 evaluate env (Let(x, exp1, exp2)) =
   evaluate env (expsubst exp2 x (Let(x, exp1, exp1)))
@@ -32,36 +32,36 @@ evaluate env (Let(x, exp1, exp2)) =
 evaluate env (Op("==", exp1, exp2)) =
   let val1 = evaluate env exp1
       val2 = evaluate env exp2
-   in case (val1, val2) of
+  in case (val1, val2) of
     (Num m, Num n) -> Boolean (m==n)
     _ -> error "Runtime type error"
 
 evaluate env (Op("<", exp1, exp2)) =
   let val1 = evaluate env exp1
       val2 = evaluate env exp2
-   in case (val1, val2) of
-        (Num m, Num n) -> Boolean (m<n)
-	_ -> error "Runtime type error"
+  in case (val1, val2) of
+    (Num m, Num n) -> Boolean (m<n)
+    _ -> error "Runtime type error"
 
 evaluate env (Op("+", exp1, exp2)) =
   let val1 = evaluate env exp1
       val2 = evaluate env exp2
-   in case (val1, val2) of
-        (Num m, Num n) -> Num (m+n)
-	_ -> error "Runtime type error"
+  in case (val1, val2) of
+    (Num m, Num n) -> Num (m+n)
+    _ -> error "Runtime type error"
 
 evaluate env (Op("-", exp1, exp2)) =
   let val1 = evaluate env exp1
       val2 = evaluate env exp2
-   in case (val1, val2) of
-        (Num m, Num n) -> Num (m-n)
-	_ -> error "Runtime type error"
+  in case (val1, val2) of
+    (Num m, Num n) -> Num (m-n)
+    _ -> error "Runtime type error"
 
 evaluate env (Op("appl", exp1, exp2)) =
   let val1 = evaluate env exp1
-   in case val1 of
-        (Lam (x, exp0)) -> evaluate env (expsubst exp0 x exp2)
-	_ -> error "Runtime type error"
+  in case val1 of
+    (Lam (x, exp0)) -> evaluate env (expsubst exp0 x exp2)
+    _ -> error "Runtime type error"
 
 evaluate env (Lam (x, exp)) = Lam (x, exp)
 
